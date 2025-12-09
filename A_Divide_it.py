d17bbb62106904ff47984ca2321a93d31e1ce5ec
AShla22-19 <zeynuahla@gmail.com>
2025-12-10 00:05:58 +0200

    CSEC CONTES

--- A_Divide_it.py
+++ A_Divide_it.py
@@ -1,15 +1,28 @@
 n = int(input())
 for i in range(n):
     m = int(input())
+    count = 0
 
-    if m % 5 ==0:
-        m = 4 * m / 2
+    while m > 1 :
+        if m % 5 == 0:
+            m = 4 * m // 5
 
-    elif m % 3 == 0:
-        m = 2 * m / 3
+        elif m % 3 == 0:
+            m = 2 * m // 3
 
-    elif m % 2 == 0:
-        m = m / 2
+        elif m % 2 == 0:
+            m = m // 2
+
+        else:
+            count = -1
+            break
+        
+        count += 1
+    
+    print(count)
+
+
+
     
   
 
